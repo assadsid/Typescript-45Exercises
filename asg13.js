576aa57c30e650c12346b86c92dd8451f590d06d
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-10 23:59:02 +0500

    Add files via upload

---
+++ asg13.js
@@ -0,0 +1,9 @@
+var names = ["Honda", "Swift", "Toyota", "KIA"];
+var arry1 = "I love to drive " + names[0] + " cars, it is very comfort to drive";
+var arry2 = "The cars manufactured by " + names[1] + " gives best fuel average";
+var arry3 = names[2] + " cars are one of the most expansive cars in the world";
+var arry4 = "One of the best sports car is " + names[3] + " sportage";
+console.log(arry1);
+console.log(arry2);
+console.log(arry3);
+console.log(arry4);
