8e925beb045172674e6bb1d90aa5075db1cc9872
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-11 13:41:26 +0500

    Update asg15.js

--- asg15.js
+++ asg15.js
@@ -1,8 +1,8 @@
 var names = ["Ali", "Saad", "Sana"];
 var message = ("I'd like to invite you a Dinner gathering at my home");
-for (var a = 0; a < names.length; a++) {
-    console.log(names[a] + ", " + message);
-}
+// for (var a=0 ; a<names.length ; a++){
+// console.log(names[a] + ", " + message);
+//}
 names[1] = "Ahmed";
 for (var a = 0; a < names.length; a++) {
     console.log(names[a] + ", " + message);
