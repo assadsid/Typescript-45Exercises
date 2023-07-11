42b81acba021b29df2a6f4dea916af8bbcdde709
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-11 21:54:43 +0500

    Add files via upload

---
+++ asg18.ts
@@ -0,0 +1,57 @@
+var places = ["Paris", "Germany", "France", "Australia", "Italy"];
+var places2 = ["Australia", "France", "Germany", "Italy", "Paris"];
+var places3 = ["Paris", "Italy", "Germany", "France", "Australia"];
+
+/*
+   for (var a=0 ; a<places.length ; a++){
+       console.log(places[a]);
+       }
+*/    
+/*
+// Array in alphabetical order
+places.sort();
+for (var a=0 ; a<places.length ; a++){
+    console.log(places[a]);
+}
+*/
+/*
+Array still in its original order
+    for (var a=0 ; a<places.length ; a++){
+        console.log(places[a]);
+        }
+*/
+/*
+// Array in reverse alphabetical order
+places.sort();
+for (var a = places.length - 1 ; a >= 0; a--){
+    console.log(places[a]);
+}
+*/
+/*
+//  Array still in its original order
+    for (var a=0 ; a<places.length ; a++){
+        console.log(places[a]);
+        }
+*/
+/*
+//  Array reverse in its original order
+    for (var a = places.length - 1 ; a >= 0; a--){
+        console.log(places[a]);
+        }
+*/
+/*
+//  Array back again in its original order
+for (var a=0 ; a<places.length ; a++){
+    console.log(places[a]);
+    }
+*/
+/*
+// Array sort in alphabetical order
+for (var a=0 ; a<places2.length ; a++){
+    console.log(places2[a]);
+    }
+*/
+// Array sort in reverse alphabetical order
+for (var a=0 ; a<places3.length ; a++){
+    console.log(places3[a]);
+    }
