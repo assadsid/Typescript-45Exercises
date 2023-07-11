02b5cfba4e6fb7ad9debc05fc1c470c0b353c19f
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-11 13:39:29 +0500

    Update asg16.js

--- asg16.js
+++ asg16.js
@@ -1,8 +1,8 @@
 var names = ["Ali", "Saad", "Sana", "Hina"];
 var message = ("I'd like to invite you a Dinner gathering at my home");
-for (var a = 0; a < names.length; a++) {
-    console.log(names[a] + ", " + message);
-}
+//  for (var a=0 ; a<names.length ; a++){
+//  console.log(names[a] + ", " + message);
+//}
 names[1] = "Ahmed";
 names[4] = "Aisha";
 for (var a = 0; a < names.length; a++) {
