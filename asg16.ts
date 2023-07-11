dcfdbda7dd31fb0f9f7b874d3a8c075b7263ecb1
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-11 13:39:00 +0500

    Update asg16.ts

--- asg16.ts
+++ asg16.ts
@@ -2,10 +2,10 @@ var names = ["Ali", "Saad", "Sana", "Hina"];
 
 var message = ("I'd like to invite you a Dinner gathering at my home");
 
-    for (var a=0 ; a<names.length ; a++){
-    console.log(names[a] + ", " + message);
-}
-    names[1] = "Ahmed"; names[4] = "Aisha";
+  //  for (var a=0 ; a<names.length ; a++){
+  //  console.log(names[a] + ", " + message);
+//}
+names[1] = "Ahmed"; names[4] = "Aisha";
 
     for (var a=0 ; a<names.length ; a++){
         console.log(names[a] + ", " + message);
