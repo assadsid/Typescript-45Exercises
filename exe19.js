0b085b4b7b568ae8ff3419a00465d57de4c6d431
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-12 11:04:40 +0500

    Add files via upload

---
+++ exe19.js
@@ -0,0 +1,6 @@
+var names = ["Ali", "Saad", "Sana", "Hina"];
+names[4] = "Ahmed";
+names[5] = "Aisha";
+for (var a = 0; a < names.length; a++) {
+    console.log(a + " " + names[a]);
+}
---
+++ exe19.ts
@@ -0,0 +1,7 @@
+var names = ["Ali", "Saad", "Sana", "Hina"];
+
+names[4] = "Ahmed"; names[5] = "Aisha";
+
+    for (var a=0 ; a<names.length ; a++){
+        console.log (a +" "+ names[a]);
+    }
