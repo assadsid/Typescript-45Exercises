57fddb279ee72d24faec439e3aa2e9ce6fa8f1f5
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-12 13:49:00 +0500

    Add files via upload

---
+++ exe20.ts
@@ -0,0 +1,15 @@
+var mountains = ["Mount Everest", "Karakoram", "Makalu", "Cho Oyu", "Nanga Parbat"];
+
+
+    for (var a=0 ; a<mountains.length ; a++){
+        console.log (a +" "+ mountains[a]);
+    }
+
+/*
+    var rivers = ["Ravi", "Chunab", "Jhelum", "Neelum", "Indus"];
+    
+    for (var a=0 ; a<rivers.length ; a++){
+        console.log (a +" "+ rivers[a]);
+    }
+*/
+
