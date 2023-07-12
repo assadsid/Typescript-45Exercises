aa38cfa2bfafbca0206e9b4ebb22cb1eac75edd8
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-12 13:12:54 +0500

    Update exe19.ts

--- exe19.ts
+++ exe19.ts
@@ -1,7 +1,12 @@
-var names = ["Ali", "Saad", "Sana", "Hina"];
-
+var names = ["Ali", "Saad", "Sana", "Hina", "Ahmed", "Sana"];
+var list = ("There are total " + names.length + " persons attending dinner gathering")
+/*
 names[4] = "Ahmed"; names[5] = "Aisha";
-
+// This will result a list format of persons.
     for (var a=0 ; a<names.length ; a++){
         console.log (a +" "+ names[a]);
     }
+*/
+
+// This will result a liner statement that how many persons are invited.
+// console.log(list);
