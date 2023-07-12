b34bbb81fbb16626de14a101c7a4e216caa3e704
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-12 10:38:20 +0500

    Add files via upload

---
+++ asg17.js
@@ -0,0 +1,20 @@
+var names = ["Saad", "Hina"];
+var message = ("I'd like to invite you a Dinner gathering at my home");
+//  for (var a=0 ; a<names.length ; a++){
+//  console.log(names[a] + ", " + message);
+//}
+names[2] = "Ahmed";
+names[3] = "Aisha";
+//  for (var a=0 ; a<names.length ; a++){
+//     console.log(names[a] + ", " + message);
+// }
+var message2 = ("I am only inviting two people for Dinner's gathering");
+console.log(message2);
+var message3 = ("I am sorry that i can't invite you to Dinner gathering");
+//   for (var a=0 ; a<names.length ; a++){
+//       console.log(names[a] + ", " + message3);
+//       }
+var names2 = ["Ali", "Sana"];
+//   for (var a=0 ; a<names2.length ; a++){
+//       console.log(names2[a] + ", " + message);
+//       }
