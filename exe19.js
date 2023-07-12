b6016360190828ed3d6ae7a3265eca26f10ce047
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-12 13:13:32 +0500

    Update exe19.js

--- exe19.js
+++ exe19.js
@@ -1,6 +1,11 @@
-var names = ["Ali", "Saad", "Sana", "Hina"];
-names[4] = "Ahmed";
-names[5] = "Aisha";
-for (var a = 0; a < names.length; a++) {
-    console.log(a + " " + names[a]);
-}
+var names = ["Ali", "Saad", "Sana", "Hina", "Ahmed", "Sana"];
+var list = ("There are total " + names.length + " persons attending dinner gathering");
+/*
+names[4] = "Ahmed"; names[5] = "Aisha";
+// This will result a list format of persons.
+    for (var a=0 ; a<names.length ; a++){
+        console.log (a +" "+ names[a]);
+    }
+*/
+// This will result a liner statement that how many persons are invited.
+// console.log(list);
