81b13b2a4ddaac280c4faaede5af2007d3262a50
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-13 11:44:15 +0500

    Update exe12.ts

--- exe12.ts
+++ exe12.ts
@@ -1,10 +1,6 @@
 var names = ["Ali", "Saad", "Sana", "Hina"];
 
-names[0] = "He is a good boy";
-names[1] = "He is a good boy";
-names[2] = "She is a good girl";
-names[3] = "She is a good girl";
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
+console.log(names[0] + " is a good boy");
+console.log(names[1] + " is a good boy");
+console.log(names[2] + " is a good girl");
+console.log(names[3] + " is a good girl");
