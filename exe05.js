ccfc447704d6068c4ad5e323fe32928b7d3968a3
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-14 16:09:45 +0500

    Update exe05.js

--- exe05.js
+++ exe05.js
@@ -1,4 +1,3 @@
-var famous_person = ("M.A. Jinnah said,");
-var message2 = ('"Failure is a word unknown to me."');
-console.log(famous_person);
-console.log(message2);
+var famous_person = "M.A. Jinnah said,";
+var message2 = '"Failure is a word unknown to me."';
+console.log("".concat(famous_person, "\n").concat(message2));
