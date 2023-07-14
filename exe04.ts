731f51829c2c0ac314fe2d9bc669bb7858aa5ef9
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-14 16:05:22 +0500

    Update exe04.ts

--- exe04.ts
+++ exe04.ts
@@ -1,2 +1,4 @@
-var fmq = ('Quaid-e-Azam once said, "I do not believe in taking the right decision, I take a decision and make it right."');
-console.log(fmq);
+let person = "Quaid-e-Azam once said,"
+let quote = '"I do not believe in taking the right decision,\nI take a decision and make it right."'
+
+console.log(`${person}\n${quote}`);
