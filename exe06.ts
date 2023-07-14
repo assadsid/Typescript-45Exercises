3086aba1e6800a3121da3445c0e8f308574e9506
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-14 16:19:58 +0500

    Update exe06.ts

--- exe06.ts
+++ exe06.ts
@@ -1,6 +1,5 @@
-var firstName = "Assad";
-var lastName = "Siddiqui";
-
-var fullName = ("\t" + firstName + "\n" + lastName);
+let firstName = "Assad";
+let lastName = "Siddiqui";
+let fullName = ("\t" + firstName + "\n" + lastName);
 
 console.log(fullName);
