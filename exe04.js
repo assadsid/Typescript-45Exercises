66614279a2f445062ca8353369c09946f078208f
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-18 10:26:23 +0500

    Update exe04.js

--- exe04.js
+++ exe04.js
@@ -1,3 +1,3 @@
-var person = "Quaid-e-Azam once said,";
-var quote = '"I do not believe in taking the right decision,\nI take a decision and make it right."';
-console.log("".concat(person, "\n").concat(quote));
+var person = "Quaid-e-Azam";
+var quote = '"I do not believe in taking the right decision, I take a decision and make it right."';
+console.log("".concat(person, " once said,\n    ").concat(quote));
