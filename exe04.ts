5d1bedd3e4d33e273cdcfedb19c6254429df2dd7
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-18 10:26:00 +0500

    Update exe04.ts

--- exe04.ts
+++ exe04.ts
@@ -1,4 +1,4 @@
-let person = "Quaid-e-Azam once said,"
-let quote = '"I do not believe in taking the right decision,\nI take a decision and make it right."'
+const person = "Quaid-e-Azam"
+const quote = '"I do not believe in taking the right decision, I take a decision and make it right."'
 
-console.log(`${person}\n${quote}`);
+console.log(`${person} once said,\n    ${quote}`);
