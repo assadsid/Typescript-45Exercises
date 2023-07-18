aa932600fa7a0c603fb3a82ebbf5afaba10b7b56
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-18 10:19:59 +0500

    Update exe03.js

--- exe03.js
+++ exe03.js
@@ -1,4 +1,4 @@
-var nm1 = ("> assadsid");
-var nm2 = ("> ASSADSID");
-var nm3 = ("> Assadsid");
-console.log("".concat(nm1, "\n").concat(nm2, "\n").concat(nm3));
+var nm2 = "Assad";
+console.log(nm2.toLowerCase());
+console.log(nm2.toUpperCase());
+console.log(nm2);
