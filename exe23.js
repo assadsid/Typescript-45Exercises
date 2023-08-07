0d978f9b018d70d0cc5aeadffae4f73ed512d2cc
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-08 00:15:26 +0500

    Add files via upload

---
+++ exe23.js
@@ -0,0 +1,20 @@
+"use strict";
+let car = "Vitz";
+if (car === "Sportage") {
+    console.log("It's true");
+}
+else if (car === "Civic") {
+    console.log("It's true");
+}
+else if (car === "Swift") {
+    console.log("It's true");
+}
+else if (car === "Vitz") {
+    console.log("It's true");
+}
+else if (car === "Vezel") {
+    console.log("It's true");
+}
+else {
+    console.log("It's false!!");
+}
---
+++ exe23.ts
@@ -0,0 +1,19 @@
+let car = "Vitz";
+
+if (car === "Sportage") {
+    console.log("It's true");
+}
+else if (car === "Civic") {
+    console.log("It's true");
+}
+else if (car === "Swift") {
+    console.log("It's true");
+}
+else if (car === "Vitz") {
+    console.log("It's true");
+}
+else if (car === "Vezel") {
+    console.log("It's true");
+} else {
+    console.log("It's false!!");
+}
