343edd976e8541d771fcf27844b471a37bcdba99
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-07 20:26:32 +0500

    Update exe20.ts

--- exe20.ts
+++ exe20.ts
@@ -1,15 +1,7 @@
-var mountains = ["Mount Everest", "Karakoram", "Makalu", "Cho Oyu", "Nanga Parbat"];
+let mountains = ["null", "Mount Everest", "Karakoram", "Makalu", "Cho Oyu", "Nanga Parbat"];
 
+console.log("The 5 famous mountains are:");
 
-    for (var a=0 ; a<mountains.length ; a++){
-        console.log (a +" "+ mountains[a]);
+for (let a=1 ; a<mountains.length ; a++) {
+    console.log (`${a} ${mountains[a]}`);
     }
-
-/*
-    var rivers = ["Ravi", "Chunab", "Jhelum", "Neelum", "Indus"];
-    
-    for (var a=0 ; a<rivers.length ; a++){
-        console.log (a +" "+ rivers[a]);
-    }
-*/
-
