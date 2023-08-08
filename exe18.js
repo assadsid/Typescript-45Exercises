cfb88f223d496aa2091edaf3865911b9f87a38c6
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-08 10:33:08 +0500

    Update exe18.js

--- exe18.js
+++ exe18.js
@@ -1,56 +1,11 @@
-var places = ["Paris", "Germany", "France", "Australia", "Italy"];
-var places2 = ["Australia", "France", "Germany", "Italy", "Paris"];
-var places3 = ["Paris", "Italy", "Germany", "France", "Australia"];
-/*
-   for (var a=0 ; a<places.length ; a++){
-       console.log(places[a]);
-       }
-*/
-/*
-// Array in alphabetical order
-places.sort();
-for (var a=0 ; a<places.length ; a++){
-    console.log(places[a]);
-}
-*/
-/*
-Array still in its original order
-    for (var a=0 ; a<places.length ; a++){
-        console.log(places[a]);
-        }
-*/
-/*
-// Array in reverse alphabetical order
-places.sort();
-for (var a = places.length - 1 ; a >= 0; a--){
-    console.log(places[a]);
-}
-*/
-/*
-//  Array still in its original order
-    for (var a=0 ; a<places.length ; a++){
-        console.log(places[a]);
-        }
-*/
-/*
-//  Array reverse in its original order
-    for (var a = places.length - 1 ; a >= 0; a--){
-        console.log(places[a]);
-        }
-*/
-/*
-//  Array back again in its original order
-for (var a=0 ; a<places.length ; a++){
-    console.log(places[a]);
-    }
-*/
-/*
-// Array sort in alphabetical order
-for (var a=0 ; a<places2.length ; a++){
-    console.log(places2[a]);
-    }
-*/
-// Array sort in reverse alphabetical order
-for (var a = 0; a < places3.length; a++) {
-    console.log(places3[a]);
-}
+let places = ["Turkey", "Malaysia", "Argentina", "Europe", "Canada"];
+console.log(places);
+let sortPlaces = [...places].sort();
+console.log(sortPlaces);
+console.log(places);
+let revPlaces = [...sortPlaces].reverse();
+console.log(revPlaces);
+console.log(places);
+let revMainPlaces = [...places].reverse();
+console.log(revMainPlaces);
+console.log(places);
