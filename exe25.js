15a29162ef5a8c76a7977469a57f574d14494a71
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-09 16:55:39 +0500

    Add files via upload

---
+++ exe25.js
@@ -0,0 +1,12 @@
+"use strict";
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("The player just earned 5 points");
+}
+let alien_color1 = "yellow";
+if (alien_color1 === "green") {
+    console.log("The player just earned 5 points");
+}
+else {
+    console.log();
+}
---
+++ exe25.ts
@@ -0,0 +1,13 @@
+let alien_color = "green"
+
+if(alien_color === "green") {
+    console.log("The player just earned 5 points");
+}
+
+let alien_color1 = "yellow"
+
+if(alien_color1 === "green") {
+    console.log("The player just earned 5 points");
+} else {
+    console.log()
+}
