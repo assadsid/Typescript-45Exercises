5e0fa26126c9919e1f342d3a552bb71bf5514895
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-09 17:30:32 +0500

    Add files via upload

---
+++ exe27.ts
@@ -0,0 +1,43 @@
+let alien_color3 = "green"
+
+if(alien_color3 === "green") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else if(alien_color3 === "yellow") {
+    console.log("The player just earned 10 points");
+}
+else if(alien_color3 === "red") {
+    console.log("The player just earned 15 points")
+} else {
+    console.log()
+}
+
+
+let alien_color3b = "yellow"
+
+if(alien_color3b === "green") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else if(alien_color3b === "yellow") {
+    console.log("The player just earned 10 points");
+}
+else if(alien_color3b === "red") {
+    console.log("The player just earned 15 points")
+} else {
+    console.log()
+}
+
+
+let alien_color3c = "red"
+
+if(alien_color3c === "green") {
+    console.log("The player just earned 5 points for shooting the alien");
+}
+else if(alien_color3c === "yellow") {
+    console.log("The player just earned 10 points");
+}
+else if(alien_color3c === "red") {
+    console.log("The player just earned 15 points")
+} else {
+    console.log()
+}
