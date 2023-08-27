fb10889727786ceef6120facb8e1eea8b7354cc4
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-27 22:48:11 +0500

    Add files via upload

---
+++ exe33.js
@@ -0,0 +1,16 @@
+"use strict";
+let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < numberArr.length; i++) {
+    if (numberArr[i] === 1) {
+        console.log("1st");
+    }
+    else if (numberArr[i] === 2) {
+        console.log("2nd");
+    }
+    else if (numberArr[i] === 3) {
+        console.log("3rd");
+    }
+    else {
+        console.log(`${numberArr[i]}th`);
+    }
+}
---
+++ exe34.js
@@ -0,0 +1,11 @@
+"use strict";
+let favPizza = ["Chicken Fajita", "Supremo Max", "Arabian Delight"];
+console.log(favPizza);
+for (let i = 0; i < favPizza.length; i++) {
+    console.log(`I like ${favPizza[i]} pizza`);
+}
+console.log(`\nHow much do you like Pizza?`);
+let rateOutput = ["Delicious", "Yummy", "Good"];
+for (let i = 0; i < rateOutput.length; i++) {
+    console.log(`The pizza was ${rateOutput[i]}`);
+}
