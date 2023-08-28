690eece4f085b4e7fc421fc621e8cfd62615f7ef
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-28 22:40:24 +0500

    Add files via upload

---
+++ exe37.js
@@ -0,0 +1,13 @@
+"use strict";
+function make_Shirt2(param1) {
+    if (param1 === "Large") {
+        console.log(`Make a ${param1} size shirt and print "I Love Typescript.`);
+    }
+    else if (param1 === "Medium") {
+        console.log(`Make a ${param1} size shirt and print "I Love Typescript.`);
+    }
+    else {
+        console.log(`Make a ${param1} size shirt and print "Pakistan Zindabad".`);
+    }
+}
+make_Shirt2("XL");
---
+++ exe37.ts
@@ -0,0 +1,11 @@
+function make_Shirt2 (param1:string ) {
+    if(param1 === "Large") {
+        console.log(`Make a ${param1} size shirt and print "I Love Typescript.`);
+    } else if (param1 === "Medium") {
+        console.log(`Make a ${param1} size shirt and print "I Love Typescript.`);
+    } else {
+        console.log(`Make a ${param1} size shirt and print "Pakistan Zindabad".`);
+    }
+}
+
+make_Shirt2("XL");
