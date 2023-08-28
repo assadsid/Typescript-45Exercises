a05c20888955f279a1e52264ddffdcc7718b8396
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-28 20:45:14 +0500

    Add files via upload

---
+++ exe36.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_Shirt(para1, para2) {
+    console.log(`Make a ${para1} size shirt and print ${para2} on it`);
+}
+make_Shirt("XXL", '"I\'m a Coder"');
---
+++ exe36.ts
@@ -0,0 +1,5 @@
+function make_Shirt (para1:string, para2:string) {
+    console.log(`Make a ${para1} size shirt and print ${para2} on it`);
+}
+
+make_Shirt("XXL", '"I\'m a Coder"');
