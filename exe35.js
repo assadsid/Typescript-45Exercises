f5710ea4a2a43ec568fef29a0092f05abf2b491c
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-28 20:31:50 +0500

    Add files via upload

---
+++ exe35.js
@@ -0,0 +1,17 @@
+"use strict";
+let pets = ["Dog", "Cat", "Parrot"];
+for (let i = 0; i < pets.length; i++) {
+    console.log(pets[i]);
+    if (pets[i] === "Dog") {
+        console.log(`A dog would make a great pet.\n`);
+    }
+    else if (pets[i] === "Cat") {
+        console.log(`Cats are quite impervious to threats.\n`);
+    }
+    else if (pets[i] === "Parrot") {
+        console.log(`A talkative parrots are funny.\n`);
+    }
+}
+let messageForPets = "What these animals have in common?";
+let answerOfMessage = "Any of these animals would make a great pet!";
+console.log(`Q. ${messageForPets}\nA. ${answerOfMessage}`);
