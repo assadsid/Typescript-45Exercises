27c53623971d0453d40ea1d3918e8e311d600043
assadsid <138975909+assadsid@users.noreply.github.com>
2023-08-29 06:44:27 +0500

    Add files via upload

---
+++ exe38.js
@@ -0,0 +1,8 @@
+"use strict";
+function describe_City(city, country = "Pakistan") {
+    console.log(`${city} city is in ${country}`);
+}
+;
+describe_City("Lahore");
+describe_City("Multan");
+describe_City("Baku", "Azerbaijan");
---
+++ exe38.ts
@@ -0,0 +1,7 @@
+function describe_City(city:string, country="Pakistan") {
+    console.log(`${city} city is in ${country}`);
+};
+
+describe_City("Lahore");
+describe_City("Multan");
+describe_City("Baku", "Azerbaijan");
